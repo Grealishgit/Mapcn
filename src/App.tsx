583367f6f6bb7ec668e44a75bf00268d272352dd
Eugene <eugyneehunter@gmail.com>
2026-01-09 01:05:10 +0300

    Update routing in App component to include MapClusters and correct route paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom'
 import MainMap from './pages/MainMap'
 import { MiniMaps } from './pages/MiniMaps'
 import { MapRoutes } from './pages/MapRoutes'
+import MapClusters from './pages/MapClusters'
 
 function App() {
 
@@ -15,7 +16,8 @@ function App() {
         {/* Define your routes here */}
         <Route path="/map" element={<MainMap />} />
         <Route path="/mini-maps" element={<MiniMaps />} />
-        <Route path="/routes" element={<MapRoutes />} />
+        <Route path="/" element={<MapRoutes />} />
+        <Route path="/clusters" element={<MapClusters />} />
       </Routes>
     </>
   )
