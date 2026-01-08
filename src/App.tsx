169e4ba44cdaed026bc96b4dbd1de1da8aa072f7
Eugene <eugyneehunter@gmail.com>
2026-01-08 23:13:25 +0300

    Remove unused React import from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 
-import React from 'react'
+// import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import MainMap from './pages/MainMap'
 import { MiniMaps } from './pages/MiniMaps'
